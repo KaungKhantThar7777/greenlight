4d9c575c328a7f619820aa3f83c8854bbd9c82e5
Kaung Khant Thar <kaungkhantthar77@gmail.com>
2023-06-29 08:27:08 +0630

    added: create movie and get movie api with httprouter

---
+++ cmd/api/movies.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Create a movie")
+
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Show the details of movie %d\n", id)
+}
